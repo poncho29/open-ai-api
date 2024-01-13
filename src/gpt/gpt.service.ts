ec5f8f274558660c7c870ca253a8042e13d62af0
Sebastian Meneses <meneses0828@gmail.com>
2024-01-12 21:44:41 -0500

    [ADD] - config instance of openai

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -3,11 +3,16 @@ import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
 
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPEN_API_KEY,
+  });
+
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({
+    return await orthographyCheckUseCase(this.openai, {
       prompt: orthographyDto.prompt,
     });
   }
